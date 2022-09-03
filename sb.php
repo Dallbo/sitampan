1fc6455d54f6b5e8fc735610bba61fa9b5404c1e
Aditya Kurnia Rahman <aditya.krohman@gmail.com>
2022-09-03 13:58:39 +0700

    Halaman Beranda

--- sb.php
+++ sb.php
@@ -1,17 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
+
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
+
     <title>SiTampan</title>
+
     <!-- Custom fonts for this template-->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
+
     <!-- Custom styles for this template-->
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
 </head>
@@ -19,7 +24,7 @@
 <body id="page-top">
     <!-- Page Wrapper -->
     <div id="wrapper">
-        <!-- Sidebar Start-->
+        <!-- Sidebar -->
         <ul class="navbar-nav  bg-gradient-primary sidebar sidebar-dark accordion" style="color:black;" id="accordionSidebar">
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
@@ -63,14 +68,15 @@
             </li>
             <!-- Divider -->
             <hr class="sidebar-divider">
+
         </ul>
-        <!-- Sidebar Stop-->
+        <!-- End of Sidebar -->
 
         <!-- Content Wrapper -->
         <div id="content-wrapper" class="d-flex flex-column">
             <!-- Main Content -->
             <div id="content">
-                <!-- Topbar Start-->
+                <!-- Topbar -->
                 <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
                     <img src="Bojonegoro.png" style="width:80px;height:60px;"/>
                     <div class="col"> 
@@ -83,7 +89,7 @@
                         </a>
                     </li>
                 </nav>
-                <!-- Topbar Stop-->
+                <!-- End of Topbar -->
 
                 <!-- Begin Page Content -->
                 <div class="container-fluid">
@@ -219,8 +225,10 @@
                 </div>
             </footer>
             <!-- End of Footer -->
+
         </div>
         <!-- End of Content Wrapper -->
+
     </div>
     <!-- End of Page Wrapper -->
 
